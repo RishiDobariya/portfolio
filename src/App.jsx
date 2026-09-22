4bac636bb574160c69f795009c48f3377a79f56f
Rishu Patel <rishupatel1011@gmail.com>
2026-09-22 15:49:28 +0530

    Fix Projects page routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
 import Resume from './pages/Resume.jsx'
 import Services from './pages/Services.jsx'
+import Portfolio from './pages/Portfolio.jsx'
+import PortfolioDetails from './pages/PortfolioDetails.jsx'
 import Contact from './pages/Contact.jsx'
 
 export default function App() {
@@ -14,6 +16,8 @@ export default function App() {
         <Route path="about" element={<About />} />
         <Route path="resume" element={<Resume />} />
         <Route path="services" element={<Services />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="portfolio-details" element={<PortfolioDetails />} />
         <Route path="contact" element={<Contact />} />
         <Route path="*" element={<Home />} />
       </Route>
